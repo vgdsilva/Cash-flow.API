58560d4d6c34cdc1f494a205775b516a80c94265
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2025-04-21 16:15:37 -0300

    Adicionar arquivos de projeto.

---
+++ CashFlow.Infrastructure/Persistence/CashFlowDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.Persistence;
+
+public class CashFlowDbContext : DbContext
+{
+    public CashFlowDbContext(DbContextOptions<CashFlowDbContext> options) : base(options) { }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
